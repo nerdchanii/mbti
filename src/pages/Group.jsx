43601eeff010fe9dacde6b47ee3e233b2af9d527
nerdchanii <nerdchanii@gmail.com>
2022-04-09 05:12:58 +0900

    add group name

--- src/pages/Group.jsx
+++ src/pages/Group.jsx
@@ -1,18 +1,20 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import Layout from '../layout';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Layout from "../layout";
 
-import { renderGraph } from '../utils/graph';
-import ShareButton from '../components/ShareButton';
-import { Button } from '@mui/material';
+import { renderGraph } from "../utils/graph";
+import ShareButton from "../components/ShareButton";
+import { Button } from "@mui/material";
 
 function Group() {
   const { groupId } = useParams();
   const [name, setName] = useState(undefined);
   useEffect(() => {
     (async () => {
-      const response = await axios(`https://mbti-api.ttbkk.com/api/mbti/${groupId}`);
+      const response = await axios(
+        `https://mbti-api.ttbkk.com/api/mbti/${groupId}`
+      );
       const data = response.data;
       console.log(data);
       setName(data.group.name);
@@ -20,15 +22,25 @@ function Group() {
     })();
   }, []);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <Layout>
-      <div id="mountNode" style={{ height: window.document.documentElement.clientWidth / 2 }}/>
-      <div style={{ textAlign: 'center' }}>
-        <Button onClick={() => navigate(`/group/${groupId}/add`)} variant={'contained'} color={'info'}
-                size={'large'}>내 MBTI 추가하기</Button>
-        <ShareButton name={name}/>
+      <h2 style={{ textAlign: "center", marginTop: "1rem" }}>{name}</h2>
+      <div
+        id="mountNode"
+        style={{ height: window.document.documentElement.clientWidth / 2 }}
+      />
+      <div style={{ textAlign: "center" }}>
+        <Button
+          onClick={() => navigate(`/group/${groupId}/add`)}
+          variant={"contained"}
+          color={"info"}
+          size={"large"}
+        >
+          내 MBTI 추가하기
+        </Button>
+        <ShareButton name={name} />
       </div>
     </Layout>
   );
